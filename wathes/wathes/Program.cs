f32ca0b618c126af7da8dcbdffa597e907233d19
egornagarnovich <egor.naganovich@gmail.com>
2018-12-20 14:04:36 +0300

    create new program

---
+++ wathes/wathes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace wathes
+{
+    public enum Time
+    {
+        hour,
+        minute,
+    }
+    public enum Arguments
+    {
+        time,
+        specificator,
+    }
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">Time and path to json specificator.</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TimeParser timeParser = new TimeParser();
+                PathToAudioFile path = new PathToAudioFile();
+                SimpleSound play = new SimpleSound();
+                string[] time = timeParser.GetHourAndMinute(ref args[0]);
+                string pathToAudioWitchHour = path.GetPath(ref time[(int)Time.hour], Time.hour,
+                                                           ref args[(int)Arguments.specificator]);
+                string pathToAudioWitchMinute = path.GetPath(ref time[(int)Time.minute], Time.minute,
+                                                                      ref args[(int)Arguments.specificator]);
+                
+                play.PlaySimpleSound(pathToAudioWitchHour);
+                System.Threading.Thread.Sleep(500);
+                play.PlaySimpleSound(pathToAudioWitchMinute);
+                System.Threading.Thread.Sleep(500);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ wathes/wathes/SimpleSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Media;
+namespace wathes
+{
+    /// <summary>
+    /// Simple sound.
+    /// </summary>
+    public class SimpleSound
+    {
+        /// <summary>
+        /// Plaies the simple sound.
+        /// </summary>
+        /// <param name="pathToAudioFile">Path to audio file.</param>
+        public void PlaySimpleSound(string pathToAudioFile)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(pathToAudioFile);  
+            simpleSound.PlaySync();  
+        }
+    }
+}
